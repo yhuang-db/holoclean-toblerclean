7144c352abf25eaf4f446cfd0d690703f495145f
Yuchuan Huang <itisyuchuan@gmail.com>
2022-10-31 09:54:31 -0500

    upload tobler detector

--- detect/__init__.py
+++ detect/__init__.py
@@ -3,6 +3,7 @@ from .detector import Detector
 from .nulldetector import NullDetector
 from .violationdetector import ViolationDetector
 from .errorloaderdetector import ErrorsLoaderDetector
+from .toblerdetector import ToblerDetector
 
-__all__ = ['DetectEngine', 'Detector', 'NullDetector', 
-            'ViolationDetector', 'ErrorsLoaderDetector']
+__all__ = ['DetectEngine', 'Detector', 'NullDetector',
+           'ViolationDetector', 'ErrorsLoaderDetector', 'ToblerDetector']
